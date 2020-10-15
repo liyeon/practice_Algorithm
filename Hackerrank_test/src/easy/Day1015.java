0227e624b43ae93aad3a57029d30bbd1dad870f7
liyeon <fldus612@gmail.com>
2020-10-15 22:17:22 +0900

    [ Feat ] Java If-else

--- Hackerrank_test/src/easy/Day1015.java
+++ Hackerrank_test/src/easy/Day1015.java
@@ -10,9 +10,10 @@ public class Day1015 {
 		int N = scanner.nextInt();
 		//1이상  100이하인 정수 에서
 		if(N>=1 || N<=100){
+			// 홀수이면 Weird 출력
             if(N%2 != 0){
                 System.out.println("Weird");
-            }else if(N>=6 && N<=20) {
+            }else if(N>=6 && N<=20) { 
                 System.out.println("Weird");
             }else if(N>=2 && N<=5) {
                 System.out.println("Not Weird");
@@ -23,5 +24,14 @@ public class Day1015 {
 
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 		scanner.close();
+		/* 테스트 케이스
+		 * 24 -> Not Weird
+		 * 4 -> Not Weird
+		 * 18 -> Weird
+		 * 29 -> Weird
+		 * 5 -> Weird
+		 * 100 -> Not Weird
+		 * 20 ->  Weird
+		 */
 	}
 }
